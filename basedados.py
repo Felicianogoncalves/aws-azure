614b82960c582a98fd090d6897b68f96cfa04dda
Feliciano Júnior Gonçalves <juniorsilvagoncalvez@gmail.com>
2025-10-28 11:40:33 +0000

    Add script to print server and user details

---
+++ basedados.py
@@ -0,0 +1,10 @@
+import sys
+
+print("Nome do script: ", sys.argv[0])
+print("Parametros: ", sys.argv[1:])
+ 
+servidor = sys.argv[1]
+user = sys.argv[2]
+password = *****
+
+print(f'O servidor é {servidor} o user é {user} e a pass é {password})
